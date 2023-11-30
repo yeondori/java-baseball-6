0959cad10bd47e4094ed57bf6b6d61d7a42df0c2
yeondori <yeonseo.jung0@gmail.com>
2023-12-01 01:14:59 +0900

    feat: 문제 생성 기능 추가

--- src/main/java/baseball/GameManager.java
+++ src/main/java/baseball/GameManager.java
@@ -1,4 +1,27 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.IntStream;
+
 public class GameManager {
+    private final List<Integer> answer;
+
+    public GameManager(List<Integer> answer) {
+        this.answer = createNumbers();
+    }
+
+    private List<Integer> createNumbers() {
+        List<Integer> numbers = new ArrayList<>();
+        while (numbers.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!numbers.contains(randomNumber)) {
+                numbers.add(randomNumber);
+            }
+        }
+        return numbers;
+    }
 }
