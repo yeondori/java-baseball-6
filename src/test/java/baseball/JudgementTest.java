157c28892b277be19cb3affb8ad1962121c07221
yeondori <yeonseo.jung0@gmail.com>
2023-12-02 17:20:39 +0900

    feat: judge 테스트(3볼인 경우)

--- src/test/java/baseball/JudgementTest.java
+++ src/test/java/baseball/JudgementTest.java
@@ -3,18 +3,16 @@ package baseball;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class JudgementTest {
     private static final List<Integer> ANSWER = Arrays.asList(1,2,3);
-    private static final List<Integer> PLAYER1 = Arrays.asList(1,2,3);
-    private static final List<Integer> PLAYER2 = Arrays.asList(3,1,2);
-    private static final List<Integer> PLAYER3 = Arrays.asList(4,5,6);
+    private static final List<Integer> ALL_STRIKES = Arrays.asList(1,2,3);
+    private static final List<Integer> ALL_BALLS = Arrays.asList(3,1,2);
+    private static final List<Integer> NOTHING = Arrays.asList(4,5,6);
 
     @Test
     @DisplayName("정답과 일치하면 3스트라이크이다.")
@@ -22,8 +20,19 @@ class JudgementTest {
         //given
         Judgement judgement = new Judgement(ANSWER);
         //when
-        Result result = judgement.Judge(PLAYER1);
+        Result result = judgement.Judge(ALL_STRIKES);
         //then
         assertThat(result.strike).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("정답과 구성은 일치하나 모든 위치가 다르면 3볼이다.")
+    public void judgeIfAllBalls() throws Exception {
+        //given
+        Judgement judgement = new Judgement(ANSWER);
+        //when
+        Result result = judgement.Judge(ALL_BALLS);
+        //then
+        assertThat(result.ball).isEqualTo(3);
     }
 }
