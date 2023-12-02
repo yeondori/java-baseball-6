9182756124e182ecbd40f0c3c2884f4c46e62b2a
yeondori <yeonseo.jung0@gmail.com>
2023-12-02 16:04:24 +0900

    feat: judge 기능 추가

--- src/main/java/baseball/Judgement.java
+++ src/main/java/baseball/Judgement.java
@@ -10,4 +10,25 @@ public class Judgement {
     public Judgement(List<Integer> answer) {
         this.answer = answer;
     }
+
+    public Result Judge(List<Integer> player) {
+        int strikeCount = countStrikes(player);
+        int ballCount = countBalls(player);
+
+        return new Result(strikeCount, ballCount);
+    }
+
+    private int countStrikes(List<Integer> player) {
+        return IntStream.range(0, player.size())
+                .filter(i -> Objects.equals(player.get(i), answer.get(i)))
+                .toArray()
+                .length;
+    }
+
+    private int countBalls(List<Integer> player) {
+        return IntStream.range(0, player.size())
+                .filter(i -> !Objects.equals(player.get(i), answer.get(i)) && answer.contains(player.get(i)))
+                .toArray()
+                .length;
+    }
 }
