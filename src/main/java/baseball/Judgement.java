d192baeb708e2a7bca79c583c079de7e656d0252
yeondori <yeonseo.jung0@gmail.com>
2023-12-02 16:03:55 +0900

    feat: judgement 생성

---
+++ src/main/java/baseball/Judgement.java
@@ -0,0 +1,13 @@
+package baseball;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.IntStream;
+
+public class Judgement {
+    private final List<Integer> answer;
+
+    public Judgement(List<Integer> answer) {
+        this.answer = answer;
+    }
+}
