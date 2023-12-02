149e90816d93d34699d00317010589c8707e7b6a
yeondori <yeonseo.jung0@gmail.com>
2023-12-02 15:48:27 +0900

    refactor: static 키워드 추가

--- src/main/java/baseball/GameBuilder.java
+++ src/main/java/baseball/GameBuilder.java
@@ -12,12 +12,12 @@ public class GameBuilder {
         this.answer = answer;
     }
 
-    public GameBuilder build() {
+    public static GameBuilder build() {
         List<Integer> answer = createNumbers();
         return new GameBuilder(answer);
     }
 
-    private List<Integer> createNumbers() {
+    private static List<Integer> createNumbers() {
         List<Integer> numbers = new ArrayList<>();
         while (numbers.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
