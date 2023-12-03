71dd54f0a787d939076da5ad684be87f01a725b3
yeondori <yeonseo.jung0@gmail.com>
2023-12-03 14:23:18 +0900

    feat: 숫자야구 프로그램 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,33 @@
 package baseball;
 
+import java.util.List;
+
 public class Application {
+    static InputView inputView = new InputView();
+    static OutputView outputView = new OutputView();
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        int restart;
+        do {
+            playGame();
+            restart = inputView.requestRestartNumber();
+        }
+        while (restart == 1);
+    }
+
+    private static void playGame() {
+        GameBuilder game = GameBuilder.build();
+        Judgement judgement = new Judgement(game.getAnswer());
+
+        while (true) {
+            List<Integer> player = inputView.requestNumbers();
+            Result result = judgement.judge(player);
+            outputView.printResult(result);
+            if (result.status().equals("FINISH")) {
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                break;
+            }
+        }
     }
 }
